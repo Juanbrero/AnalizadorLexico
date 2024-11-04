57725b6d67918e19f8d26ae5b9c788cb5b395aad
Juan Brero <juaan.b17@hotmail.com>
2024-11-04 18:49:17 -0300

    Correccion de reglas gramaticales

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // Iniciamos el programa
-        new Vista();
+//        new Vista();
+        new VistaSarachoUseWithCare();
     }
 }
